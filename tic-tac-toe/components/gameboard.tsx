35e1c09ee1de29b6ec11fde47d38cdbe539d7778
Rimmy Le <56008781+rimmyle@users.noreply.github.com>
2022-11-28 12:47:16 -0800

    Board Build

--- tic-tac-toe/components/gameboard.tsx
+++ tic-tac-toe/components/gameboard.tsx
@@ -1,5 +1,31 @@
 function gameBoard() {
-    return (<h1> this is the game board lol</h1>)
+
+
+    return (
+        <>
+            <h1> this is the game board lol</h1>
+            <section className="gameBoard">
+                <li>
+                    <div className="boardBox">Bitch Lasagna</div>
+                    <div className="boardBox">asdfasdfa</div>
+                    <div className="boardBox">Bitch Lasagna</div>
+                </li>
+                <li>
+                    <div className="boardBox">Bitch Lasagna</div>
+                    <div className="boardBox">Bitch Lasagna</div>
+                    <div className="boardBox">Bitch Lasagna</div>
+                </li>
+                <li>
+                    <div className="boardBox">Bitch Lasagna</div>
+                    <div className="boardBox">Bitch Lasagna</div>
+                    <div className="boardBox">Bitch Lasagna</div>
+                </li>
+            </section>
+                
+            
+        </>
+    
+    )
 }
 
 export default gameBoard()
