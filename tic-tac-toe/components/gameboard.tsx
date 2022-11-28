69a4323328efe86e1693dd147f765ae7260000fe
Rimmy Le <56008781+rimmyle@users.noreply.github.com>
2022-11-28 12:38:23 -0800

    gameBoard in index

---
+++ tic-tac-toe/components/gameboard.tsx
@@ -0,0 +1,5 @@
+function gameBoard() {
+    return (<h1> this is the game board lol</h1>)
+}
+
+export default gameBoard()
--- tic-tac-toe/pages/game.tsx
+++ tic-tac-toe/pages/game.tsx
@@ -1,5 +1,12 @@
+import gameBoard from "../components/gameboard"
 function gamePage() {
-    return (<h1>Hello</h1>)
+    return (
+    <>
+        <h1>Hello</h1>
+        {gameBoard}
+    </>
+    
+    )
 }
 
 export default gamePage
